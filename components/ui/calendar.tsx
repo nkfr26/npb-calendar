36db5298c37b830366e5bb0f682da843aaa53f88
nkfr26 <n@rfu.jp>
2025-08-20 03:27:24 +0900

    nav style

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -51,7 +51,7 @@ function Calendar({
         ),
         month: cn("flex flex-col w-full gap-4", defaultClassNames.month),
         nav: cn(
-          "flex items-center gap-1 w-full absolute top-0 inset-x-0 justify-between",
+          "flex items-center gap-1 w-full absolute top-0 inset-x-0 justify-end",
           defaultClassNames.nav,
         ),
         button_previous: cn(
