61ed5e3cf9598fc32af6d9b452bb2557ca379994
nkfr26 <n@rfu.jp>
2025-09-09 22:02:04 +0900

    wrap suspense

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,16 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import type { ReactNode } from "react";
+import { type ReactNode, Suspense } from "react";
 import { getQueryClient } from "../lib/get-query-client";
 
 export function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <NuqsAdapter>{children}</NuqsAdapter>
+      <Suspense>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </Suspense>
     </QueryClientProvider>
   );
 }
