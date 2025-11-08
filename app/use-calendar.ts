65d4cbf0e828e7246f9d8e65927b17b2b7929e5d
nkfr26 <n@rfu.jp>
2025-11-09 02:26:56 +0900

    fix

--- app/use-calendar.ts
+++ app/use-calendar.ts
@@ -5,7 +5,14 @@ export const useCalendar = () => {
   const [selected, setSelected] = useState<Date | undefined>(undefined);
   const [month, setMonth] = useState(new Date());
   const onMonthChange: MonthChangeEventHandler = (month) => {
-    setMonth(month);
+    const monthNumber = month.getMonth();
+    if (monthNumber === 11) {
+      setMonth(new Date(month.getFullYear() + 1, 2));
+    } else if (monthNumber === 1) {
+      setMonth(new Date(month.getFullYear() - 1, 10));
+    } else {
+      setMonth(month);
+    }
     setSelected(undefined);
   };
   return { selected, onSelect: setSelected, month, onMonthChange };
