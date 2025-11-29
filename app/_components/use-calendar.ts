183d0353dacbfc5d0a2f48dddda98b726ad73436
nkfr26 <n@rfu.jp>
2025-11-30 01:30:42 +0900

    fix month

--- app/_components/use-calendar.ts
+++ app/_components/use-calendar.ts
@@ -1,9 +1,21 @@
 import { useState } from "react";
 import type { MonthChangeEventHandler } from "react-day-picker";
 
+const getInitialMonth = (month: Date) => {
+  const monthNumber = month.getMonth() + 1;
+
+  if (monthNumber === 12) {
+    return new Date(month.getFullYear() + 1, 2);
+  } else if (monthNumber === 1 || monthNumber === 2) {
+    return new Date(month.getFullYear(), 2);
+  }
+  return month;
+};
+
 export const useCalendar = () => {
   const [selected, setSelected] = useState<Date | undefined>(undefined);
-  const [month, setMonth] = useState(new Date());
+  const [month, setMonth] = useState(() => getInitialMonth(new Date()));
+
   const onMonthChange: MonthChangeEventHandler = (month) => {
     const monthNumber = month.getMonth();
     if (monthNumber === 11) {
