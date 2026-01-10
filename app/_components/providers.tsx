01686a53588722b4448a2ef87c3d8871187d8d6f
nkfr26 <n@rfu.jp>
2026-01-10 21:03:04 +0900

    fix

--- app/_components/providers.tsx
+++ app/_components/providers.tsx
@@ -3,7 +3,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { type ReactNode, Suspense } from "react";
-import { getQueryClient } from "../lib/get-query-client";
+import { getQueryClient } from "@/lib/get-query-client";
 
 export function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
