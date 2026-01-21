67026ccab92386b54f731a900de11d29ad1b3f9d
nkfr26 <n@rfu.jp>
2026-01-21 21:50:46 +0900

    temporal-polyfill

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,14 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Temporal } from "temporal-polyfill";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: Date) {
-  return date
-    .toLocaleDateString("ja-JP", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    })
-    .replaceAll("/", "-");
+export function formatDate(date: Date): string {
+  return Temporal.Instant.from(date.toISOString())
+    .toZonedDateTimeISO(Temporal.Now.timeZoneId())
+    .toPlainDate()
+    .toString();
 }
